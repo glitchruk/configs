dadc031a9b9703b4708d641dc89d16774897a2c5
GlitchR <me@glitchr.uk>
2022-07-14 20:01:03 +0100

    feat(java_selector): Begin implementation

---
+++ scripts/rc/java_selector.sh
@@ -0,0 +1,38 @@
+# .==[ RC Script ]==(v0.1.0)==============================.
+# |                                                       |
+# |    Name:  Java Version Selector                       |
+# |  Author:  GlitchR                                     |
+# |                                                       |
+# +=-[ Description ]-------------------------------------=+
+# |                                                       |
+# |    Provides functions for listing, and selecting,     |
+# |  versions installed in a users' personal home         |
+# |  directory.                                           |
+# |                                                       |
+# |    Currently allows for a Java version to be set      |
+# |  "globally" for all bash sessions; and for an         |
+# |  alternative version to be selected for the current   |
+# |  session.                                             |
+# |                                                       |
+# +=-[ Changelog ]---------------------------------------=+
+# |                                                       |
+# |  --.-(v0.1.0)                                         |
+# |    |                                                  |
+# |    |-: Initial implementation of script; added the    |
+# |    |   basic required features.                       |
+# |    |                                                  |
+# |    |-> Implement "jlist";                             |
+# |    |-> Implement "jset";                              |
+# |    `-> Implement "juse".                              |
+# |                                                       |
+# '======================================================='
+
+# .--[ Function ]--( jlist )------------------------------.
+# |                                                       |
+# |    Lists all currently installed versions of Java in  |
+# |  the users' home directory.                           |
+# |                                                       |
+# '-------------------------------------------------------'
+
+jlist() {
+}
